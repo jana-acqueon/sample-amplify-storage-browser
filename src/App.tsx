ae9e3b21413d4d7786fc51f2e27644d2d4a6ecdc
Jana <jana.g@acqueon.com>
2025-08-21 18:15:42 +0530

    update the app.tsx with new changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,33 @@ import './App.css';
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button } from '@aws-amplify/ui-react';
+import { fetchAuthSession } from 'aws-amplify/auth';
+
 Amplify.configure(config);
+
+async function getResolvedFolders() {
+  const session = await fetchAuthSession();
+  const identityId = session.identityId; // <- Cognito Identity ID
+
+  return [
+    { path: 'public/', label: 'Public' },
+    { path: `private/${identityId}/`, label: 'My Private Files' }, // 👈 replaces {entity_id}
+    { path: 'admin/', label: 'Admin Area' },
+  ];
+}
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
+  getFolders: getResolvedFolders, // 👈 forces resolved folders
 });
 
 function App() {
   return (
-    <Authenticator>
+    <Authenticator hideSignUp={true}>
       {({ signOut, user }) => (
         <>
           <div className="header">
-            <h1>{`Hello ${user?.username}`}</h1>
+            <h1>{`Hello ${user?.signInDetails?.loginId || user?.username}`}</h1>
             <Button onClick={signOut}>Sign out</Button>
           </div>
           <StorageBrowser />
