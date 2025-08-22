7183fd7b8f52d9bd7eea168885e915befecb3320
Jana <jana.g@acqueon.com>
2025-08-22 09:04:25 +0530

    Updated the code to show the all available folders in S3 bucket configured

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,69 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { storage, secondaryStorage } from './storage/resource';
+import { storage } from './storage/resource';
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
-
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
-  storage,
-  secondaryStorage
 });
+
+
+const customBucketStack = backend.createStack("custom-bucket-stack");
+
+// Import existing bucket
+const customBucket = Bucket.fromBucketAttributes(customBucketStack, "MyCustomBucket", {
+  bucketArn: "arn:aws:s3:::ps-amplify-bucket ",
+  region: "us-east-1"
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: customBucket.env.region,
+    bucket_name: customBucket.bucketName,
+    buckets: [
+      {
+        aws_region: customBucket.env.region,
+        bucket_name: customBucket.bucketName,
+        name: customBucket.bucketName,
+        paths: {
+          "": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write", "delete"],
+          },
+        },
+      }
+    ]
+  },
+});
+
+// ... Unauthenticated/guest user policies and role attachments go here ...
+
+/*
+  Define an inline policy to attach to Amplify's auth role
+  This policy defines how authenticated users can access your existing bucket
+*/
+const authPolicy = new Policy(backend.stack, "customBucketAuthPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject"
+      ],
+      resources: [`${customBucket.bucketArn}/*`,],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:ListBucket"],
+      resources: [
+        `${customBucket.bucketArn}`,
+        `${customBucket.bucketArn}/*`
+      ],
+    }),
+  ],
+});
+
+// Add the policies to the authenticated user role
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(authPolicy);
